e7de83b85ac2ac2adc03bf6b15203fa1428f7c0a
Tomek28611 <tomek286@yahoo.com>
2023-02-17 09:02:22 +0100

    justify

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-center  font-bold px-10 py-5 mb-10">
       <div>
         <h1 className="sm:text-2xl md:text-4xl pb-4 ml-2">Welcome to Andaman Rental Motorbikes</h1>
         <h2 className="mt-5 md:mt-0">
